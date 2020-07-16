0185e5197a781d3e25cb4f70e158cb108f56750f
unknown <rasika.jadhav7@gmail.com>
2020-07-16 11:07:13 +0530

    assignment checkin

---
+++ demo/demo/src/main/java/com/tavisca/assignment/exceptions/IllegalIngredientsException.java
@@ -0,0 +1,8 @@
+package com.tavisca.assignment.exceptions;
+
+public class IllegalIngredientsException extends RuntimeException {
+
+    public IllegalIngredientsException(String message) {
+        super(message);
+    }
+}
---
+++ demo/demo/src/test/java/com/tavisca/assignment/demo/BevergesDemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.tavisca.assignment.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tavisca.assignment.beveragesTest.CRUDOpertions;
+import com.tavisca.assignment.exceptions.InvalidOrderException;
+
+@SpringBootTest
+class BevergesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Mock
+	CRUDOpertions crudOp;
+	
+	
+	
+}
